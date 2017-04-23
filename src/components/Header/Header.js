f34a37fc22fc6c8d0a2075575b64557f54c388af
Sandra <s.kriemann@mediosmanagement.de>
2017-04-23 16:13:14 +0200

    add logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,12 +19,12 @@ function Header() {
       <div className={s.container}>
         <Navigation className={s.nav} />
         <Link className={s.brand} to="/">
-          <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-          <span className={s.brandTxt}>Your Company</span>
+          <img src={require('./sonic-deployment-system.png')} width="45" height="38" alt="React" />
+          <span className={s.brandTxt}>Sonic Mobile Deployment</span>
         </Link>
         <div className={s.banner}>
-          <h1 className={s.bannerTitle}>React</h1>
-          <p className={s.bannerDesc}>Complex web apps made easy</p>
+          <h1 className={s.bannerTitle}>Sonic</h1>
+          <p className={s.bannerDesc}>Mobile Deployment made easy and fast</p>
         </div>
       </div>
     </div>
